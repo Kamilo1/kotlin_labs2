a6b04f2884d915d1eb86670ef6ffc410148714af
Kamilo1 <kamil.panus@wp.pl>
2024-04-17 16:33:15 +0200

    Form and List

---
+++ app/src/main/java/pl/wsei/lab06/MainActivity.kt
@@ -0,0 +1,253 @@
+package pl.wsei.lab06
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import pl.wsei.lab06.ui.theme.Lab06Theme
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab06Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(
+    navController: NavController,
+    title: String,
+    showBackIcon: Boolean,
+    route: String) {
+    TopAppBar(
+        colors = TopAppBarDefaults.largeTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        ),
+        title = { Text(text = title) },
+        navigationIcon = {
+            if (showBackIcon) {
+                IconButton(onClick = { navController.navigate(route) }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        },
+        actions = {
+            if (route !== "form") {
+                OutlinedButton(
+                    onClick = { navController.navigate("list") }
+                )
+                {
+                    Text(
+                        text = "Zapisz",
+                        fontSize = 18.sp
+                    )
+                }
+            } else {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = "")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = "")
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            AppTopBar(
+                navController = navController,
+                title = "List",
+                showBackIcon = false,
+                route = "form"
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                shape = CircleShape,
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add task",
+                        modifier = Modifier.scale(1.5f)
+                    )
+                },
+                onClick = {
+                    navController.navigate("form")
+                }
+            )
+        },
+        //pozostałe parametry
+        content = {
+            LazyColumn(modifier = Modifier.padding(it)) {
+                items(items = todoTasks()) { item ->
+                    ListItem(item = item)
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            AppTopBar(
+                navController = navController,
+                title = "Form",
+                showBackIcon = true,
+                route = "list"
+            )
+        },
+        content = { innerPadding ->
+            Surface(modifier = Modifier.padding(innerPadding)) {
+                Text("Formularz")
+            }
+        }
+    )
+}
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "list") {
+        composable("list") { ListScreen(navController = navController) }
+        composable("form") { FormScreen(navController = navController) }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    Lab06Theme {
+        MainScreen(
+        )
+    }
+}
+@Composable
+fun ListItem(item: TodoTask, modifier: Modifier = Modifier) {
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = item.title,
+                    style = MaterialTheme.typography.titleLarge,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                Text(
+                    text = item.deadline.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Priority: ${item.priority}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = if (item.isDone) "Completed" else "Not Completed",
+                    color = if (item.isDone) Color(0xFF4CAF50) else MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+fun todoTasks(): List<TodoTask> {
+    return listOf(
+        TodoTask("Programming", LocalDate.of(2024, 4, 18), false, Priority.Low),
+        TodoTask("Teaching", LocalDate.of(2024, 5, 12), false, Priority.High),
+        TodoTask("Learning", LocalDate.of(2024, 6, 28), true, Priority.Low),
+        TodoTask("Cooking", LocalDate.of(2024, 8, 18), false, Priority.Medium),
+    )
+}
+
+enum class Priority() {
+    High, Medium, Low
+}
+
+data class TodoTask(
+    val title: String,
+    val deadline: LocalDate,
+    val isDone: Boolean,
+    val priority: Priority
+)
